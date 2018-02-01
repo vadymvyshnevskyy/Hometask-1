2f28766ac96ab9ff1b7e33d10804efa8264f75ad
neverlandjt <33659777+neverlandjt@users.noreply.github.com>
2018-02-01 11:09:48 +0200

    Update n2.py

--- n2.py
+++ n2.py
@@ -6,26 +6,22 @@ def input_inf():
     Input information about amount of chocolates, radius of box
     and the accuracy of the calculations
     '''
-    # check the input
-    try:
-        # create variables and ask user to input required information
-        n = int(input("Input amount of chocolates: "))
-        r = int(input("Input radius of box: "))
-        tch = float(input("Input the accuracy of the calculations: "))
-        if n < 0 or r < 0:
-            print("Wrong input")
-            return 0
-        l, s1, s2 = [], [], []
-        for i in range(n):
-            a1, a2, a3 = map(int, input("Enter Triples of value:\n \
+    # create variables and ask user to input required information
+    n = int(input("Input amount of chocolates: "))
+    r = int(input("Input radius of box: "))
+    tch = float(input("Input the accuracy of the calculations: "))
+    if n < 0 or r < 0:
+        print("Wrong input")
+        return 0
+    l, s1, s2 = [], [], []
+    for i in range(n):
+        a1, a2, a3 = map(int, input("Enter Triples of value:\n \
 Length of rope, Area of first chocolate bar, \
 Area of second chocolate bar: ").split())
-            l.append(a1)
-            s1.append(a2)
-            s2.append(a3)
-        return n, r, tch, l, s1, s2
-    except:
-        print("Wrong input")
+        l.append(a1)
+        s1.append(a2)
+        s2.append(a3)
+    return n, r, tch, l, s1, s2
 
 
 def cycle_len(r):
@@ -63,13 +59,18 @@ def check(r, tch, l, s1, s2):
 
 
 def main_program():
-    n, r, tch, l, s1, s2 = input_inf()
-    res = []
-    for i in range(n):
-        if check(r, tch, l[i], s1[i], s2[i]):
-            res.append((l[i], s1[i], s2[i]))
-    # write in file the result
-    write_file(res)
-
+    try:
+        n, r, tch, l, s1, s2 = input_inf()
+        print(s1)
+        print(s2)
+        res = []
+        for i in range(n):
+            if check(r, tch, l[i], s1[i], s2[i]):
+                res.append((l[i], s1[i], s2[i]))
+        # print and write in file the result
+        print (res)
+        write_file(res)
+    except:
+        print("Wrong input!")
 
 main_program()
